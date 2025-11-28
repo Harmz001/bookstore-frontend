4cc529945eb3d2145aa3f185936fb7b154b2a654
Harmz001 <abdulham33d017@gmail.com>
2025-11-28 23:01:52 +0400

    chore: add vue.config.js and 404.html for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  lintOnSave: false,
-  devServer: {
-    port: 8080,
-    open: true,
-    host: 'localhost',
-    client: {
-      webSocketURL: 'ws://localhost:8080/ws'
-    }
-  }
-}
+  publicPath: process.env.NODE_ENV === 'production' ? '/bookstore-frontend/' : '/'
+};
